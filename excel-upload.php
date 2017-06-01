0a4cfed79b9be01c2ec9b69d6340f330eb4b02cc
Serge ANDRIAMAHEFARIVO <sandriamahefarivo@ingenosya.mg>
2017-06-01 10:12:57 +0300

    Test script upload

--- excel-upload.php
+++ excel-upload.php
@@ -4,8 +4,8 @@ include 'libraries/PHPExcel/IOFactory.php';
 
 $server = "localhost";
 $username = "root";
-$password = "";
-$database = "obsfoncier";
+$password = "root";
+$database = "obs_foncier";
 
 mysql_connect($server, $username, $password);
 mysql_select_db($database);
