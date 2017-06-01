29f7b8afe9412512a6ca2a8356210b8ee6682d95
Guillaume SERGENT - Tanyah Consulting <ingenosya.guillaume@gmail.com>
2017-06-01 14:52:01 +0200

    blocks

--- modules/entreprise/src/Plugin/Block/VwPermisautorisation.php
+++ modules/entreprise/src/Plugin/Block/VwPermisautorisation.php
@@ -2,7 +2,7 @@
 
 /*
  * @file
- * Contains \Drupal\entreprise\Plugin\Block\VwPermisminier
+ * Contains \Drupal\entreprise\Plugin\Block\VwPermisautorisation
  */
 
 namespace Drupal\entreprise\Plugin\Block;
@@ -13,11 +13,11 @@ use Drupal\Core\Link;
 /**
  * @Block (
  *  id = "VwPermisminier",
- *  admin_label = @Translation("VwPermisminier"),
+ *  admin_label = @Translation("VwPermisautorisation"),
  *  categroy = @Translation("ObsFoncier")
  * )
  */
-class VwPermisminier extends BlockBase {
+class VwPermisautorisation extends BlockBase {
 	
 	/**
 	 * {@inheritdoc}
@@ -26,14 +26,14 @@ class VwPermisminier extends BlockBase {
 		// check si le user est un ministere
 		if (!in_array('ministere', \Drupal::currentUser()->getRoles())
 				&& !in_array('adminfoncier', \Drupal::currentUser()->getRoles()) ) {
-					return ['#markup' => '<hr />'];
-				}
+			return ['#markup' => '<hr />'];
+		}
 		
-		$vw = views_embed_view('liste_des_permis_miniers', 'block_1', ac_getParentPrincipalCurrentUser());
+		$vw = views_embed_view('liste_des_permis_autorisation', 'block_1', ac_getParentPrincipalCurrentUser());
 		$renderview = \Drupal::service('renderer')->render($vw);
 		
 		$link = '<div class="BtnCommander">'
-				. Link::createFromRoute($this->t('Ajouter un permis minier'), 'adulte_form.form', array('identity' => 'add'), array('attributes' => array('class' => array('btn', 'btn-default'))))->toString()
+				. Link::createFromRoute($this->t('Ajouter un permis autorisation environnemental'), 'adulte_form.form', array('identity' => 'add'), array('attributes' => array('class' => array('btn', 'btn-default'))))->toString()
 				. '</div>';
 				
 				return [
